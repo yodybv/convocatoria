8764a8cabdfd25bf8b455d515705ca74c210341d
yodybv <36285195+yodybv@users.noreply.github.com>
2025-11-24 13:38:55 -0500

    se modifico gitingori,next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: "export",
+  trailingSlash: true,
+  distDir: "rrhhdeploy",
   images: {
     unoptimized: true,
+    domains: [
+      "sistemas.diresahuanuco.gob.pe",
+      "diresahuanuco.gob.pe",
+      "recursoshumanos.diresahuanuco.gob.pe",
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
   },
 };
 
